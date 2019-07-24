825500731064dec1797214535dbca09b1e1f4749
Incaze <e.kalabkin@gmail.com>
2019-07-25 00:41:28 +0300

    Подключен VK API и осуществлен переход на androidx (зачем?)

--- app/src/main/java/com/incaze/friendscheckervk/MainActivity.kt
+++ app/src/main/java/com/incaze/friendscheckervk/MainActivity.kt
@@ -2,11 +2,34 @@ package com.incaze.friendscheckervk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+/*
+import com.vk.api.sdk.VK
+import com.vk.api.sdk.auth.VKScope
+import com.vk.api.sdk.auth.VKAccessToken
+import android.content.Intent
+import com.vk.api.sdk.auth.VKAuthCallback
+*/
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //VK.login(MainActivity, arrayListOf(VKScope.WALL, VKScope.PHOTOS))
     }
+
+    /*override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val callback = object: VKAuthCallback {
+            override fun onLogin(token: VKAccessToken) {
+                // User passed authorization
+            }
+
+            override fun onLoginFailed(errorCode: Int) {
+                // User didn't pass authorization
+            }
+        }
+        if (data == null || !VK.onActivityResult(requestCode, resultCode, data, callback)) {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }*/
 }
